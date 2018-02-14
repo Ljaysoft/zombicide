331b0b9b8f2177d3ac80667be42b084508457d32
JFCaron <jfrancois.c@gmail.com>
2018-02-13 22:08:49 -0500

    GameObject class

---
+++ scripts/gamelogic/gameObject.js
@@ -0,0 +1,42 @@
+class GameObject{
+    constructor(type,name,position,imgSrc,$object){
+        GameObject.prototype.lastId++;
+        this.id = GameObject.prototype.lastId;
+        this.type = type ? type : GameObject.getTypes().UNDEFINED;
+        this.name = name ? name : "UnKnownEntity";
+        this.position = position ? position : {
+            xTile : -1,
+            yTile : -1
+        };
+        this.imgSrc = imgSrc ? imgSrc : "assets/unkownEntity.png";
+        this.$object = $object ? $object : GameObject.getTemplate(this);
+    }
+
+    static getTemplate(gameobject) {
+        let $template = $('<div class="gameObject'+ gameobject.type + 
+                         '><img src="'+gameobject.imgSrc+'" /></div>');
+        let types = GameObject.getTypes();
+        switch(gameobject.type){
+            case types.HERO:
+                break;
+            case types.ZOMBIE:
+                break;
+            default:
+        }
+        return $template.clone();
+    }
+
+    static newObject(type,name,position,imgSrc,$object){
+        return new GameObject(type,name,position,imgSrc,$object);
+    };
+
+    static getTypes(){
+        return {
+            UNDEFINED: "",
+            HERO: "hero",
+            ZOMBIE: "zombie"
+        };
+    }
+}
+
+GameObject.prototype.lastId = 0;
